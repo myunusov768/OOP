4fa251137c35f0da0abde2610d01c39c12ac6da7
myunusov768 <118165862+myunusov768@users.noreply.github.com>
2023-01-16 16:45:10 +0500

    Test modifiers Access

---
+++ MyLibrary/Class1.cs
@@ -0,0 +1,43 @@
+﻿namespace MyLibrary
+{
+    class ADefoult
+    {
+        public string aPublic = "public";
+        private string aPrivate = "private";
+        internal string aInterbal = "internal";
+        protected string aProtected = "protected";
+        protected internal string aProtectedInternal = "protected internal";
+        private protected string aPrivateProtected = "private protacted";
+
+        public void PrintPublic() { Console.WriteLine(aPublic); }
+        private void PrintPrivate() { Console.WriteLine(aPrivate); }
+        protected void PrintProtected() { Console.WriteLine(aProtected); }
+        internal void PrintInternal() { Console.WriteLine(aInterbal); }
+        protected internal void PrintProtectedInternal() { Console.WriteLine(aProtectedInternal); }
+        private protected void PrintPrivateProtected() { Console.WriteLine(aPrivateProtected); }
+
+    }
+    class DefoultB : ADefoult
+    {
+        ADefoult aDefoult = new ADefoult();
+
+        public void Print()
+        {
+            //Console.WriteLine(aPublic);
+            //Console.WriteLine(aPrivate);
+            //Console.WriteLine(aProtected);
+            //Console.WriteLine(aProtectedInternal);
+            //Console.WriteLine(aPrivateProtected);
+            ////------------------------------------
+            //aDefoult.PrintPublic();
+            //aDefoult.PrintPrivate();
+            //aDefoult.PrintProtected();
+            //aDefoult.PrintProtectedInternal();
+            //aDefoult.PrintPrivateProtected();
+
+
+        }
+
+
+    }
+}
